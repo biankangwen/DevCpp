dc20ecddbf8a4f65f46b9f1e32fcaf8ed5f279be
biankangwen@qq.com <biankangwen@qq.com>
2015-10-28 01:20:52 +0800

    优化函数名称；合并测试牌型判定和测试牌型比较。

--- shuker_work/main.cpp
+++ shuker_work/main.cpp
@@ -286,7 +286,7 @@ private:
 };
 
 //打印一手牌
-void prt5Card(vector<myCard> &vecCards) {
+void prtCards(vector<myCard> &vecCards) {
 	for(unsigned int ui=0;ui<vecCards.size();ui++) {
 		cout << vecCards[ui].prtCard() << endl;
 	}
@@ -583,34 +583,34 @@ static texasHands oTexasHands;
  *并按可比较的点数降序排列 如3333K 22Q84
  *A=14 K=13 Q=12 J=11
  */
-int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
+int getHand(vector<myCard> &vecCards, vector<int> &vecSortedFigures) {
 	if(vecCards.size() != 5) {
 		cout << "---error: [getHand]a invalid cards num:" << vecCards.size() << "---" << endl;
 		return 0;
 	}
 	
-	vecFigures.clear();
+	vecSortedFigures.clear();
 	
 	int iHand = 0;
 	int iTJQKA = 0;
 	if(isRoyalStraightFlush(vecCards)) {
 		iHand = 10;
 		//cout << "---RoyalStraightFlush---" << endl;
-		vecFigures.push_back(14);
-		vecFigures.push_back(13);
-		vecFigures.push_back(12);
-		vecFigures.push_back(11);
-		vecFigures.push_back(10);
+		vecSortedFigures.push_back(14);
+		vecSortedFigures.push_back(13);
+		vecSortedFigures.push_back(12);
+		vecSortedFigures.push_back(11);
+		vecSortedFigures.push_back(10);
 	}
 	else if(isStraightFlush(vecCards, iTJQKA)) {  //iTJQKA must equal 0
 		iHand = 9;
 		//cout << "---StraightFlush---" << endl;
-		vecFigures.push_back(vecCards[0].getFigure());
-		vecFigures.push_back(vecCards[1].getFigure());
-		vecFigures.push_back(vecCards[2].getFigure());
-		vecFigures.push_back(vecCards[3].getFigure());
-		vecFigures.push_back(vecCards[4].getFigure());
-		sort(vecFigures.begin(), vecFigures.end(), greater<int>());
+		vecSortedFigures.push_back(vecCards[0].getFigure());
+		vecSortedFigures.push_back(vecCards[1].getFigure());
+		vecSortedFigures.push_back(vecCards[2].getFigure());
+		vecSortedFigures.push_back(vecCards[3].getFigure());
+		vecSortedFigures.push_back(vecCards[4].getFigure());
+		sort(vecSortedFigures.begin(), vecSortedFigures.end(), greater<int>());
 	}
 	else if(isFourOfAKind(vecCards)) {
 		iHand = 8;
@@ -629,18 +629,18 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 		//mapFigureCnt一定是[4|1 OR 1|4]
 		it=mapFigureCnt.begin();
 		if(it->second == 4) {
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back((++it)->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back((++it)->first);
 		}
 		else {
-			vecFigures.push_back((++it)->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back((--it)->first);
+			vecSortedFigures.push_back((++it)->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back((--it)->first);
 		}
 	}
 	else if(isFullHouse(vecCards)) {
@@ -660,47 +660,47 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 		//mapFigureCnt一定是[3|2 OR 2|3]
 		it=mapFigureCnt.begin();
 		if(it->second == 3) {
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back((++it)->first);
-			vecFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back((++it)->first);
+			vecSortedFigures.push_back(it->first);
 		}
 		else {
-			vecFigures.push_back((++it)->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back(it->first);
-			vecFigures.push_back((--it)->first);
-			vecFigures.push_back(it->first);
+			vecSortedFigures.push_back((++it)->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back(it->first);
+			vecSortedFigures.push_back((--it)->first);
+			vecSortedFigures.push_back(it->first);
 		}
 	}
 	else if(isFlush(vecCards)) {
 		iHand = 6;
 		//cout << "---Flush---" << endl;
-		vecFigures.push_back(vecCards[0].getFigure()==1?14:vecCards[0].getFigure());
-		vecFigures.push_back(vecCards[1].getFigure()==1?14:vecCards[1].getFigure());
-		vecFigures.push_back(vecCards[2].getFigure()==1?14:vecCards[2].getFigure());
-		vecFigures.push_back(vecCards[3].getFigure()==1?14:vecCards[3].getFigure());
-		vecFigures.push_back(vecCards[4].getFigure()==1?14:vecCards[4].getFigure());
-		sort(vecFigures.begin(), vecFigures.end(), greater<int>());
+		vecSortedFigures.push_back(vecCards[0].getFigure()==1?14:vecCards[0].getFigure());
+		vecSortedFigures.push_back(vecCards[1].getFigure()==1?14:vecCards[1].getFigure());
+		vecSortedFigures.push_back(vecCards[2].getFigure()==1?14:vecCards[2].getFigure());
+		vecSortedFigures.push_back(vecCards[3].getFigure()==1?14:vecCards[3].getFigure());
+		vecSortedFigures.push_back(vecCards[4].getFigure()==1?14:vecCards[4].getFigure());
+		sort(vecSortedFigures.begin(), vecSortedFigures.end(), greater<int>());
 	}
 	else if(isStraight(vecCards, iTJQKA)) {
 		iHand = 5;
 		//cout << "---Straight---" << endl;
 		if(iTJQKA) {
-			vecFigures.push_back(14);
-			vecFigures.push_back(13);
-			vecFigures.push_back(12);
-			vecFigures.push_back(11);
-			vecFigures.push_back(10);
+			vecSortedFigures.push_back(14);
+			vecSortedFigures.push_back(13);
+			vecSortedFigures.push_back(12);
+			vecSortedFigures.push_back(11);
+			vecSortedFigures.push_back(10);
 		}
 		else {
-			vecFigures.push_back(vecCards[0].getFigure());
-			vecFigures.push_back(vecCards[1].getFigure());
-			vecFigures.push_back(vecCards[2].getFigure());
-			vecFigures.push_back(vecCards[3].getFigure());
-			vecFigures.push_back(vecCards[4].getFigure());
-			sort(vecFigures.begin(), vecFigures.end(), greater<int>());
+			vecSortedFigures.push_back(vecCards[0].getFigure());
+			vecSortedFigures.push_back(vecCards[1].getFigure());
+			vecSortedFigures.push_back(vecCards[2].getFigure());
+			vecSortedFigures.push_back(vecCards[3].getFigure());
+			vecSortedFigures.push_back(vecCards[4].getFigure());
+			sort(vecSortedFigures.begin(), vecSortedFigures.end(), greater<int>());
 		}
 	}
 	else if(isThreeOfAKind(vecCards)) {
@@ -723,9 +723,9 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 		int fig_1_b = 0;  //fig_1_b>fig_1_s
 		for(it=mapFigureCnt.begin();it!=mapFigureCnt.end();it++) {
 			if(it->second == 3) {
-				vecFigures.push_back(it->first);
-				vecFigures.push_back(it->first);
-				vecFigures.push_back(it->first);
+				vecSortedFigures.push_back(it->first);
+				vecSortedFigures.push_back(it->first);
+				vecSortedFigures.push_back(it->first);
 			}
 			else {
 				if(fig_1_s == 0) {
@@ -736,8 +736,8 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 				}
 			}
 		}
-		vecFigures.push_back(fig_1_b);
-		vecFigures.push_back(fig_1_s);
+		vecSortedFigures.push_back(fig_1_b);
+		vecSortedFigures.push_back(fig_1_s);
 	}
 	else if(isTwoPairs(vecCards)) {
 		iHand = 3;
@@ -763,17 +763,17 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 					fig_2 = it->first;
 				}
 				else {
-					vecFigures.push_back(it->first);
-					vecFigures.push_back(it->first);
+					vecSortedFigures.push_back(it->first);
+					vecSortedFigures.push_back(it->first);
 				}
 			}
 			else {
 				fig_1 = it->first;
 			}
 		}
-		vecFigures.push_back(fig_2);
-		vecFigures.push_back(fig_2);
-		vecFigures.push_back(fig_1);
+		vecSortedFigures.push_back(fig_2);
+		vecSortedFigures.push_back(fig_2);
+		vecSortedFigures.push_back(fig_1);
 	}
 	else if(isOnePair(vecCards)) {
 		iHand = 2;
@@ -796,8 +796,8 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 		int fig_1_b = 0;  //fig_1_b > fig_1_m > fig_1_s
 		for(it=mapFigureCnt.begin();it!=mapFigureCnt.end();it++) {
 			if(it->second == 2) {
-				vecFigures.push_back(it->first);
-				vecFigures.push_back(it->first);
+				vecSortedFigures.push_back(it->first);
+				vecSortedFigures.push_back(it->first);
 			}
 			else {
 				if(fig_1_s == 0) {
@@ -811,19 +811,19 @@ int getHand(vector<myCard> &vecCards, vector<int> &vecFigures) {
 				}
 			}
 		}
-		vecFigures.push_back(fig_1_b);
-		vecFigures.push_back(fig_1_m);
-		vecFigures.push_back(fig_1_s);
+		vecSortedFigures.push_back(fig_1_b);
+		vecSortedFigures.push_back(fig_1_m);
+		vecSortedFigures.push_back(fig_1_s);
 	}
 	else {
 		iHand = 1;
 		//cout << "---HighHand---" << endl;
-		vecFigures.push_back(vecCards[0].getFigure()==1?14:vecCards[0].getFigure());
-		vecFigures.push_back(vecCards[1].getFigure()==1?14:vecCards[1].getFigure());
-		vecFigures.push_back(vecCards[2].getFigure()==1?14:vecCards[2].getFigure());
-		vecFigures.push_back(vecCards[3].getFigure()==1?14:vecCards[3].getFigure());
-		vecFigures.push_back(vecCards[4].getFigure()==1?14:vecCards[4].getFigure());
-		sort(vecFigures.begin(), vecFigures.end(), greater<int>());
+		vecSortedFigures.push_back(vecCards[0].getFigure()==1?14:vecCards[0].getFigure());
+		vecSortedFigures.push_back(vecCards[1].getFigure()==1?14:vecCards[1].getFigure());
+		vecSortedFigures.push_back(vecCards[2].getFigure()==1?14:vecCards[2].getFigure());
+		vecSortedFigures.push_back(vecCards[3].getFigure()==1?14:vecCards[3].getFigure());
+		vecSortedFigures.push_back(vecCards[4].getFigure()==1?14:vecCards[4].getFigure());
+		sort(vecSortedFigures.begin(), vecSortedFigures.end(), greater<int>());
 	}
 	
 	return iHand;
@@ -838,12 +838,12 @@ int compareHands(vector<myCard> &vecCards_A, vector<myCard> &vecCards_B) {
 	
 	int iHand_A = 0;
 	int iHand_B = 0;
-	vector<int> vecFigures_A;
-	vector<int> vecFigures_B;
-	iHand_A = getHand(vecCards_A, vecFigures_A);
-	iHand_B = getHand(vecCards_B, vecFigures_B);
-	if(iHand_A==0 || iHand_B==0 || vecFigures_A.size()!=5 || vecFigures_B.size()!=5) {
-		cout << "---error: [compareHands]a invalid getHand ret:" << iHand_A << "|" << iHand_B << "; vecF_sz:" << vecFigures_A.size() << vecFigures_B.size() << "---" << endl;
+	vector<int> vecSortedFigures_A;
+	vector<int> vecSortedFigures_B;
+	iHand_A = getHand(vecCards_A, vecSortedFigures_A);
+	iHand_B = getHand(vecCards_B, vecSortedFigures_B);
+	if(iHand_A==0 || iHand_B==0 || vecSortedFigures_A.size()!=5 || vecSortedFigures_B.size()!=5) {
+		cout << "---error: [compareHands]a invalid getHand ret:" << iHand_A << "|" << iHand_B << "; vecF_sz:" << vecSortedFigures_A.size() << vecSortedFigures_B.size() << "---" << endl;
 		return -1;
 	}
 	
@@ -855,10 +855,10 @@ int compareHands(vector<myCard> &vecCards_A, vector<myCard> &vecCards_B) {
 	}
 	else {
 		for(int i=0;i<5;i++) {
-			if(vecFigures_A[i] > vecFigures_B[i]) {
+			if(vecSortedFigures_A[i] > vecSortedFigures_B[i]) {
 				return 2;
 			}
-			else if(vecFigures_A[i] < vecFigures_B[i]) {
+			else if(vecSortedFigures_A[i] < vecSortedFigures_B[i]) {
 				return 1;
 			}
 			else {
@@ -903,7 +903,7 @@ void myPokerTestB() {
     myCard oCard3(9,"c");vec.push_back(oCard3);
     myCard oCard4(13,"c");vec.push_back(oCard4);
     myCard oCard5(12,"c");vec.push_back(oCard5);
-	prt5Card(vec);
+	prtCards(vec);
 	
 	int iTJQKA = 0;
 	if(isRoyalStraightFlush(vec)) {
@@ -1029,30 +1029,49 @@ void myPokerTestC() {
     return;
 }
 
-//测试牌型计算函数
+//测试牌型计算函数&牌型比较函数
 void myPokerTestD() {
     cout << "---###---func myPokerTestD() end---" << endl;
 	
 	//-------指定牌型-------
-	/* vector<myCard> vecC;
-    myCard oCard1(5,"c");vecC.push_back(oCard1);
-    myCard oCard2(2,"c");vecC.push_back(oCard2);
-    myCard oCard3(7,"c");vecC.push_back(oCard3);
-    myCard oCard4(2,"s");vecC.push_back(oCard4);
-    myCard oCard5(5,"h");vecC.push_back(oCard5);
-	prt5Card(vecC);
-	vector<int> vecF;
-	int ret = getHand(vecC, vecF);
-	cout << oTexasHands.getTexasHands(ret) << "=" << vecF[0] << "|" << vecF[1] << "|" << vecF[2] << "|" << vecF[3] << "|" << vecF[4] << endl; */
+	/* int ret = 0;
+	vector<myCard> vecC_A;
+    myCard oCard1_A(5,"c");vecC_A.push_back(oCard1_A);
+    myCard oCard2_A(2,"c");vecC_A.push_back(oCard2_A);
+    myCard oCard3_A(7,"c");vecC_A.push_back(oCard3_A);
+    myCard oCard4_A(2,"s");vecC_A.push_back(oCard4_A);
+    myCard oCard5_A(5,"h");vecC_A.push_back(oCard5_A);
+	prtCards(vecC_A);
+	vector<int> vecF_A;
+	ret = getHand(vecC_A, vecF_A);
+	cout << oTexasHands.getTexasHands(ret) << "=" << vecF_A[0] << "|" << vecF_A[1] << "|" << vecF_A[2] << "|" << vecF_A[3] << "|" << vecF_A[4] << endl;
+	vector<myCard> vecC_B;
+    myCard oCard1_B(5,"c");vecC_B.push_back(oCard1_B);
+    myCard oCard2_B(2,"c");vecC_B.push_back(oCard2_B);
+    myCard oCard3_B(7,"c");vecC_B.push_back(oCard3_B);
+    myCard oCard4_B(2,"s");vecC_B.push_back(oCard4_B);
+    myCard oCard5_B(5,"h");vecC_B.push_back(oCard5_B);
+	prtCards(vecC_B);
+	vector<int> vecF_B;
+	ret = getHand(vecC_B, vecF_B);
+	cout << oTexasHands.getTexasHands(ret) << "=" << vecF_B[0] << "|" << vecF_B[1] << "|" << vecF_B[2] << "|" << vecF_B[3] << "|" << vecF_B[4] << endl;
+	
+	ret = compareHands(vecC_A, vecC_B);
+	if(ret == 1) {cout << "===[A<B]===" << endl;}
+	else if(ret == 2) {cout << "===[A>B]===" << endl;}
+	else if(ret == 0) {cout << "===[A=B]===" << endl;}
+	else {cout << "===error===" << endl;} */
 	//-------指定牌型-------
 	
 	//-------随机牌型-------
 	myPoker oPoker(52);
-	vector<myCard> vecC_r;
-	vector<int> vecF_r;
+	vector<myCard> vecC_r_A;
+	vector<int> vecF_r_A;
+	vector<myCard> vecC_r_B;
+	vector<int> vecF_r_B;
 	int ret_r = 0;
 	int iX = 1;
-	int iY = 3;  //应该 大大小于'MAX_RAND' 同一轮内(同一iX下循环iY次)不能一直使用shuffle函数返回的rand序列作为种子
+	int iY = 5;  //应该 大大小于'MAX_RAND' 同一轮内(同一iX下循环iY次)不能一直使用shuffle函数返回的rand序列作为种子
 	
 	unsigned int uiSeed = time(NULL);
 	for(int i=0;i<iX;i++) {
@@ -1062,26 +1081,27 @@ void myPokerTestD() {
 		}
 		for(int j=1;j<=iY;j++) {
 			uiSeed = oPoker.shuffle(uiSeed);  //每次洗牌都换种子(shuffle函数返回的rand序列) 如果是固定种子(同一time(NULL)下循环)则洗牌序列相同 多次洗牌会导致牌被洗回原样(导致所出现的牌型固定有N种)
-			oPoker.get5Cards(vecC_r);
-			prt5Card(vecC_r);
-			ret_r = getHand(vecC_r, vecF_r);
-			cout << oTexasHands.getTexasHands(ret_r) << "=" << vecF_r[0] << "|" << vecF_r[1] << "|" << vecF_r[2] << "|" << vecF_r[3] << "|" << vecF_r[4] << endl;
+			oPoker.get5Cards(vecC_r_A);
+			prtCards(vecC_r_A);
+			ret_r = getHand(vecC_r_A, vecF_r_A);
+			cout << oTexasHands.getTexasHands(ret_r) << "=" << vecF_r_A[0] << "|" << vecF_r_A[1] << "|" << vecF_r_A[2] << "|" << vecF_r_A[3] << "|" << vecF_r_A[4] << endl;
+			
+			uiSeed = oPoker.shuffle(uiSeed);  //每次洗牌都换种子(shuffle函数返回的rand序列) 如果是固定种子(同一time(NULL)下循环)则洗牌序列相同 多次洗牌会导致牌被洗回原样(导致所出现的牌型固定有N种)
+			oPoker.get5Cards(vecC_r_B);
+			prtCards(vecC_r_B);
+			ret_r = getHand(vecC_r_B, vecF_r_B);
+			cout << oTexasHands.getTexasHands(ret_r) << "=" << vecF_r_B[0] << "|" << vecF_r_B[1] << "|" << vecF_r_B[2] << "|" << vecF_r_B[3] << "|" << vecF_r_B[4] << endl;
+			
+			ret_r = compareHands(vecC_r_A, vecC_r_B);
+			if(ret_r == 1) {cout << "===[A<B]===" << endl;}
+			else if(ret_r == 2) {cout << "===[A>B]===" << endl;}
+			else if(ret_r == 0) {cout << "===[A=B]===" << endl;}
+			else {cout << "===error===" << endl;}
 		}
 	}
 	//-------随机牌型-------
     
     cout << "---###---func myPokerTestD() end---" << endl;
-    system("PAUSE");
-    return;
-}
-
-//测试牌型比较函数
-void myPokerTestE() {
-    cout << "---###---func myPokerTestE() end---" << endl;
-	
-	
-    
-    cout << "---###---func myPokerTestE() end---" << endl;
     system("PAUSE");
     return;
 }
@@ -1098,7 +1118,6 @@ int main(int argc, char *argv[])
       //myPokerTestB();
 	  //myPokerTestC();
 	  myPokerTestD();
-	  //myPokerTestE();
       
     system("PAUSE");
     return EXIT_SUCCESS;
